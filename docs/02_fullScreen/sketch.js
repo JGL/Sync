98165f0e77bca218e9b3f1d22d9a485b59ec224d
JGL <me@joelgethinlewis.com>
2020-08-17 18:07:44 +0100

    Working fullscreen demo with scaling

--- docs/02_fullScreen/sketch.js
+++ docs/02_fullScreen/sketch.js
@@ -13,63 +13,67 @@ PoseNet example using p5.js
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-let video;
+let capture;
 let poseNet;
 let poses = [];
 let canvas;
-let cameraWidth;
-let cameraHeight;
-
-function windowResized() {
-  console.log('resized');
-  resizeCanvas(windowWidth, windowHeight);
-}
+let captureWidth;
+let captureHeight;
+let horizontalRatio;
+let verticalRatio;
 
 function setup() {
+  cameraWidth = 640;
+  cameraHeight = 480;
+
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.position(0, 0);
   canvas.style('z-index', '-1');
   canvas.style('width', '100%');
   canvas.style('height', '100%');
+  /* via https://github.com/CodingTrain/website/blob/master/Q_and_A/Q_6_p5_background/sketch.js and https://www.youtube.com/watch?v=OIfEHD3KqCg */
   //https://github.com/processing/p5.js/wiki/Beyond-the-canvas
   // canvas.parent("p5jsSketch");
-  //createCanvas(640, 480);
 
-  cameraWidth = 640;
-  cameraHeight = 480;
-  video = createCapture(VIDEO);
-  video.size(cameraWidth, cameraHeight);
+  capture = createCapture(VIDEO);
+  capture.size(cameraWidth, cameraHeight);
 
   // Create a new poseNet method with a single detection
-  poseNet = ml5.poseNet(video, modelReady);
+  poseNet = ml5.poseNet(capture, modelReady);
   // This sets up an event that fills the global variable "poses"
   // with an array every time new poses are detected
   poseNet.on('pose', function (results) {
     poses = results;
   });
   // Hide the video element, and just show the canvas
-  video.hide();
+  capture.hide();
 
   //https://github.com/CodingTrain/website/blob/master/Q_and_A/Q_6_p5_background/sketch.js
   // mic = new p5.AudioIn();
   // mic.start();
+  horizontalRatio = windowWidth / cameraWidth;
+  verticalRatio = windowHeight / cameraHeight;
 }
 
 function modelReady() {
   select('#status').html('Model Loaded');
+  console.log("Model ready!");
 }
 
 function draw() {
-  background(0, 0, 0);
-  stroke(255);
-  image(video, 0, 0, windowWidth, windowHeight);
+  background(0);
+  image(capture, 0, 0, windowWidth, windowHeight);
 
   // We can call both functions to draw all keypoints and the skeletons
-  drawKeypoints();
-  drawSkeleton();
+  // drawKeypoints();
+  // drawSkeleton();
+
+  drawKeypointsScaled();
+  drawSkeletonScaled();
 
   //https://github.com/CodingTrain/website/blob/master/Q_and_A/Q_6_p5_background/sketch.js
   if (mouseIsPressed) {
+    stroke(255, 255, 255);
     line(pmouseX, pmouseY, mouseX, mouseY);
   }
   // var vol = mic.getLevel();
@@ -110,14 +114,47 @@ function drawSkeleton() {
   }
 }
 
-function keyPressed() {
-  clear();
+// A function to draw ellipses over the detected keypoints
+function drawKeypointsScaled() {
+  // Loop through all the poses detected
+  for (let i = 0; i < poses.length; i++) {
+    // For each pose detected, loop through all the keypoints
+    let pose = poses[i].pose;
+    for (let j = 0; j < pose.keypoints.length; j++) {
+      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+      let keypoint = pose.keypoints[j];
+      // Only draw an ellipse is the pose probability is bigger than 0.2
+      if (keypoint.score > 0.2) {
+        fill(255, 0, 0);
+        noStroke();
+        ellipse(keypoint.position.x * horizontalRatio, keypoint.position.y * verticalRatio, 10, 10);
+      }
+    }
+  }
 }
 
-function draw() {
-  if (mouseIsPressed) {
-    line(pmouseX, pmouseY, mouseX, mouseY);
+// A function to draw the skeletons
+function drawSkeletonScaled() {
+  // Loop through all the skeletons detected
+  for (let i = 0; i < poses.length; i++) {
+    let skeleton = poses[i].skeleton;
+    // For every skeleton, loop through all body connections
+    for (let j = 0; j < skeleton.length; j++) {
+      let partA = skeleton[j][0];
+      let partB = skeleton[j][1];
+      stroke(255, 0, 0);
+      line(partA.position.x * horizontalRatio, partA.position.y * verticalRatio, partB.position.x * horizontalRatio, partB.position.y * verticalRatio);
+    }
   }
-  // var vol = mic.getLevel();
-  // ellipse(width / 2, height / 2, vol * width);
+}
+
+// function keyPressed() {
+//   clear();
+// }
+
+function windowResized() {
+  console.log("Resize!");
+  resizeCanvas(windowWidth, windowHeight);
+  horizontalRatio = windowWidth / cameraWidth;
+  verticalRatio = windowHeight / cameraHeight;
 }
